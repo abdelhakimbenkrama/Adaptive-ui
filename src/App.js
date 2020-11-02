bc86a487042790d7b1e1a1c1bb7cd391c1e326b3
abdelhakimbenkrama <abdalhakim.3.benkrama1@gmail.com>
2020-11-02 11:03:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <SubContainer>
+        <Navbar />
+        <Map />
+      </SubContainer>
+      <Sidebar>
+        <MiniNavbar />
+      </Sidebar>
+    </Container>
   );
 }
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  @media (max-width: 900px) {
+    flex-direction: column;
+  }
+`;
 
+const SubContainer = styled.div`
+  width: 75%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  @media (max-width: 900px) {
+    width: 100%;
+    height: 70%;
+  }
+`;
+
+const Navbar = styled.div`
+  background-color: #9965e6;
+  height: 4rem;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+`;
+
+const Map = styled.div`
+  width: 100%;
+  flex: 1;
+  background-color: #6a79d9;
+`;
+
+const Sidebar = styled.div`
+  background-color: #65bae6;
+  height: 100%;
+  flex: 1;
+  @media (max-width: 900px) {
+    height: 400px;
+    width: 100%;
+    flex: 1;
+  }
+`;
+const MiniNavbar = styled.div`
+  background-color: #9965e6;
+  height: 4rem;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+`;
 export default App;
